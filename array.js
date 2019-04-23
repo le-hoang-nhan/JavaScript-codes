781c583b4cb2d5895193dd65b97b0d10fe52920b
Le Hoang Nhan <46989247+le-hoang-nhan@users.noreply.github.com>
2019-04-23 17:24:40 +0200

    Create array.js

---
+++ array.js
@@ -0,0 +1,29 @@
+var myArray = [];
+myArray;
+
+var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday'];
+daysOfTheWeek;
+
+var myArray = [
+0, 1, 2,
+'string1', 'string2', 'string3',
+true, false
+];
+myArray;
+
+var counties = [
+	'Belknap',
+	'Strafford',
+	'Carroll',
+	'Rockingham'
+];
+counties;
+
+var arrayOfStuff = [
+	{'name' : 'value'},
+	[1, 2, 3],
+	true,
+	'nifty'
+];
+arrayOfStuff;
+arrayOfStuff.length;
