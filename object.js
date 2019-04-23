9f5c5b581f9a36cb637537d6388b674643d1cf40
Le Hoang Nhan <46989247+le-hoang-nhan@users.noreply.github.com>
2019-04-23 17:23:12 +0200

    Create object.js

---
+++ object.js
@@ -0,0 +1,28 @@
+var bird = {
+	genus : 'corvus',
+	species : 'corvax',
+	commonName: 'raven',
+	callType : 'squawky',
+	quote : 'Nevermore',
+	maxOffspring : 5,
+	noisy : true,
+	deadly : false
+};
+
+bird.quote;
+
+bird."quote"; // this does not work
+
+bird["quote"];
+
+bird.color = "black";
+
+bird;
+
+bird["where it lives"] = "in a tree";
+bird.whereItLives = "in a tree";
+bird.whereItLives;
+bird['whereItLives'];
+
+delete bird.color;
+bird;
